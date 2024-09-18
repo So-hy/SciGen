7456166f302293af5be098e94ab6c470afca7c14
So-hy <168037454+So-hy@users.noreply.github.com>
2024-09-18 23:14:51 +0900

    Update lightning_base.py

--- baselines/lightning_base.py
+++ baselines/lightning_base.py
@@ -119,15 +119,18 @@ class BaseTransformer(pl.LightningModule):
         
         return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
         
-    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, second_order_closure=None):
-        # 일반적인 optimizer step
-        optimizer.step(closure=second_order_closure)
-        
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure=None):
+        # optimizer_closure가 None이 아닐 경우 이를 사용
+        if optimizer_closure is not None:
+            optimizer.step(closure=optimizer_closure)
+        else:
+            optimizer.step()
         optimizer.zero_grad()  # gradient 초기화
-    
-        # lr_scheduler가 단일 객체일 경우
-        if hasattr(self, 'lr_scheduler') and self.lr_scheduler is not None:
-            self.lr_scheduler.step()
+        
+        # lr_schedulers가 존재하는 경우 step 호출
+        if self.lr_schedulers():
+            for lr_scheduler in self.lr_schedulers():
+                lr_scheduler.step()
     
     def get_progress_bar_dict(self):
         running_train_loss = self.trainer.running_loss.mean()
