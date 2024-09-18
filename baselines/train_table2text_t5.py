d6b80b06d432cb41da45dc8020e273c8b224fd23
So-hy <168037454+So-hy@users.noreply.github.com>
2024-09-18 22:14:40 +0900

    Update train_table2text_t5.py

--- baselines/train_table2text_t5.py
+++ baselines/train_table2text_t5.py
@@ -27,7 +27,7 @@ class SummarizationTrainer(BaseTransformer):
 
     def __init__(self, hparams):
         super().__init__(hparams, num_labels=None, mode=self.mode)
-
+        self.validation_step_outputs = []  # 여기에 추가해라
         self.metrics_save_path = Path(self.hparams.output_dir) / "metrics.json"
         self.hparams_save_path = Path(self.hparams.output_dir) / "hparams.pkl"
         self.step_count = 0
@@ -155,6 +155,26 @@ class SummarizationTrainer(BaseTransformer):
         # validation_step_outputs 초기화
         self.validation_step_outputs.clear()
 
+    def on_test_epoch_end(self):
+    """테스트 에포크 종료 시 처리"""
+        if len(self.validation_step_outputs) == 0:
+            return
+    
+        val_loss_mean = torch.stack([x["val_loss"] for x in self.validation_step_outputs]).mean()
+        predictions = [x["preds"] for x in self.validation_step_outputs]
+        targets = [x["target"] for x in self.validation_step_outputs]
+    
+        # BLEU 스코어 계산
+        bleu_info = eval_sacre_bleu(targets, predictions)
+        moverScore = eval_mover_score(targets, predictions)
+    
+        # 결과 로깅
+        self.log("test_loss", val_loss_mean)
+        self.log("bleu_score", bleu_info)
+        self.log("mover_score", moverScore)
+    
+        # validation_step_outputs 초기화
+        self.validation_step_outputs.clear()
 
     def get_dataloader(self, type_path: str, batch_size: int, shuffle: bool = False) -> DataLoader:
         dataset = AgendaDataset(self.tokenizer, type_path=type_path, **self.dataset_kwargs)
